584861f4fef86e12310bf9a3b9aa5653d727c319
Andres Medina <amedina@pivotal.io>
2018-07-17 10:40:10 -0700

    fix contract test

--- src/api.rs
+++ src/api.rs
@@ -13,9 +13,7 @@ fn info(location: Location, client: State<Client>) -> Result<Json<String>, Strin
     client.info(location).map(Json)
 }
 
-// If run using `cargo test` then they will be run in "unit test" mode and use a mock client
-// If run using `cargo contract-test` then they will be run in "contract test" mode and do a real network call
-#[cfg(test)]
+#[cfg(all(test, not(feature = "contract")))]
 mod test {
     use super::*;
 
@@ -44,6 +42,21 @@ mod test {
                 longitude: 47.59
             }
         );
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type(), Some(ContentType::JSON));
+    }
+}
+
+#[cfg(all(test, feature = "contract"))]
+mod contract_test {
+    use rocket::http::{ContentType, Status};
+
+    #[test]
+    fn info_route() {
+        let client = ::client();
+        let response = client
+            .get("/api/info?latitude=-122.33&longitude=47.59")
+            .dispatch();
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.content_type(), Some(ContentType::JSON));
     }
