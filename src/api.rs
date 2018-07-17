85fefb58f90bbb831a719ed6de97ee24e0233bb8
Andres <andresnrx@gmail.com>
2018-07-17 09:31:21 -0700

    use mocktopus for mocking the client layer in the api module

--- src/api.rs
+++ src/api.rs
@@ -2,7 +2,7 @@ use client::Client;
 
 use rocket::{response::content::Json, State};
 
-#[derive(FromForm, Debug, Clone, Copy)]
+#[derive(FromForm, Debug, Clone, Copy, PartialEq)]
 pub struct Location {
     pub latitude: f64,
     pub longitude: f64,
@@ -17,14 +17,33 @@ fn info(location: Location, client: State<Client>) -> Result<Json<String>, Strin
 // If run using `cargo contract-test` then they will be run in "contract test" mode and do a real network call
 #[cfg(test)]
 mod test {
+    use super::*;
+
+    use mocktopus::mocking::{MockResult, Mockable};
     use rocket::http::{ContentType, Status};
 
     #[test]
     fn info_route() {
+        let mut location: Option<Location> = None;
+        unsafe {
+            Client::info.mock_raw(|_, loc| {
+                location = Some(loc);
+                MockResult::Return(Ok("Hello".to_string()))
+            });
+        }
+
         let client = ::client();
         let response = client
             .get("/api/info?latitude=-122.33&longitude=47.59")
             .dispatch();
+        let location = location.expect("Client::Info not called");
+        assert_eq!(
+            location,
+            Location {
+                latitude: -122.33,
+                longitude: 47.59
+            }
+        );
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.content_type(), Some(ContentType::JSON));
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 #![feature(custom_derive)]
+#![cfg_attr(
+    all(test, not(feature = "contract")),
+    feature(proc_macro, proc_macro_mod, proc_macro_gen)
+)]
 
 extern crate chrono;
 #[macro_use]
@@ -20,7 +24,15 @@ extern crate url;
 #[cfg(all(test, not(feature = "contract")))]
 extern crate serde_urlencoded;
 
+#[cfg(all(test, not(feature = "contract")))]
+extern crate mocktopus;
+
+#[cfg(all(test, not(feature = "contract")))]
+use mocktopus::macros::mockable;
+
 mod api;
+
+#[cfg_attr(all(test, not(feature = "contract")), mockable)]
 mod client;
 
 fn rocket() -> rocket::Rocket {
