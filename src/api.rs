c2728337d80e925055422c867ac7257835cd1994
Andres <andresnrx@gmail.com>
2018-07-25 08:39:13 -0700

    extracted how to convert from Result<T, String> into Result<Json<T>, status::Custom<String>>

--- src/api.rs
+++ src/api.rs
@@ -3,9 +3,7 @@ use client::{self, Client};
 use rocket::{http::Status, response::status, State};
 use rocket_contrib::Json;
 
-use std::result::Result as StdResult;
-
-type Result<T> = StdResult<Json<T>, status::Custom<String>>;
+type ApiResult<T> = Result<Json<T>, status::Custom<String>>;
 
 #[derive(FromForm, Debug, Clone, Copy, PartialEq)]
 pub struct Area {
@@ -16,17 +14,22 @@ pub struct Area {
 }
 
 #[get("/bus_stops?<area>")]
-fn bus_stops(client: State<Client>, area: Area) -> Result<Vec<client::BusStopInfo>> {
-    client
-        .bus_stops(area)
-        .map(Json)
-        .map_err(|e| status::Custom(Status::InternalServerError, e))
+fn bus_stops(client: State<Client>, area: Area) -> ApiResult<Vec<client::BusStopInfo>> {
+    client.bus_stops(area).into_api()
 }
 
 #[get("/bus_stop_status/<stop_id>")]
-fn status(client: State<Client>, stop_id: String) -> Result<client::BusStopStatus> {
-    client
-        .bus_stop_status(stop_id.as_ref())
-        .map(Json)
-        .map_err(|e| status::Custom(Status::InternalServerError, e))
+fn status(client: State<Client>, stop_id: String) -> ApiResult<client::BusStopStatus> {
+    client.bus_stop_status(stop_id.as_ref()).into_api()
+}
+
+trait IntoApiResult<T> {
+    fn into_api(self) -> ApiResult<T>;
+}
+
+impl<T> IntoApiResult<T> for Result<T, String> {
+    fn into_api(self) -> ApiResult<T> {
+        self.map(Json)
+            .map_err(|e| status::Custom(Status::InternalServerError, e))
+    }
 }
